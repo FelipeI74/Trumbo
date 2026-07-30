e783f312b52990e14d5e83649fcf2a9649d3bdb4
FelipeI74 <innocenti.felipe@gmail.com>
2026-07-30 14:21:44 -0400

    Add BlockType enum for different block types

---
+++ engine/core/block_type.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    HEADING = "heading"
+    ACTION = "action"
+    CHARACTER = "character"
+    DIALOGUE = "dialogue"
+    PARENTHETICAL = "parenthetical"
+    TRANSITION = "transition"
+    SHOT = "shot"
+    LYRICS = "lyrics"
